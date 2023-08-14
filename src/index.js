ee6ddfcee15f4052fca8c5d3abfecabe4b7cd2f1
Arthur Iorbalidi <arturiorbalidi01052005@gmail.com>
2023-08-14 14:09:25 +0300

    init: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+
+  let bracketMap = new Map(bracketsConfig);
+
+  for (let char of str) {
+      if (bracketMap.has(char) && (stack.length === 0 || stack[stack.length - 1] !== char)) {
+        stack.push(char);
+      } else if (stack.length === 0 || char !== bracketMap.get(stack.pop())) { 
+        return false;
+      }
+  }
+
+  return stack.length === 0;
 }
